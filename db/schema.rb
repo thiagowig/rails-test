dffd1e34a6272f6fde032e93dd78a114d4c7831a
thiago <dev.thiago@gmail.com>
2014-05-06 09:03:14 -0300

    'Safety'

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140501151159) do
+ActiveRecord::Schema.define(version: 20140506112353) do
 
   create_table "rooms", force: true do |t|
     t.string   "title"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20140501151159) do
   create_table "users", force: true do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
     t.string   "location"
     t.text     "bio"
     t.string   "test"
